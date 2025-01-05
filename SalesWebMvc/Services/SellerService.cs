650284b418bcb8d4fe927c366ddf68a5a42b401f
Vinícius Maurer <viniciusmaurer8@gmail.com>
2025-01-05 12:37:49 -0300

    Delete seller

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -1,5 +1,7 @@
 ﻿using SalesWebMvc.Data;
 using SalesWebMvc.Models;
+using System.Linq;
+using System;
 
 namespace SalesWebMvc.Services
 {
@@ -20,6 +22,18 @@ namespace SalesWebMvc.Services
         public void Insert(Seller obj)
         {
             _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
     }
